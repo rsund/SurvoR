d17c1d9926c90b04b0448de1e28fd404b9b95244
sund <sund@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-28 21:10:36 +0000

    Muste 0.1.3

--- pkg/R/MUSTE.R
+++ pkg/R/MUSTE.R
@@ -6,11 +6,11 @@ require(tcltk)
 execute <- function(code) {
   e <- try(parse(text=code))
   if (inherits(e, "try-error")) {
-    cat("-----","Syntax error while executing from editor:",code,sep="\n")
+    cat("-----","Syntax error while executing from MUSTE:",code,sep="\n")
     return()
   }
-  cat("-----","Executing from editor:",code,"-----","result:", sep="\n")
-  print(eval.parent(e))
+  cat("-----","Executing from MUSTE:",code,"-----","result:", sep="\n")
+  print(eval(e, envir=muste))
 }
 
 getCursor <- function() {
@@ -67,10 +67,6 @@ create.editfield <- function(width,rows,shadowrows) {
   editfield<<-matrix(nrow=rows,ncol=width)
   for (i in 1:width) for (j in 1:rows) editfield[j,i]<<-c(" ")
   for (j in 1:rows) editfield[j,1]<<-c("*")
-  editfield.showy<<-1
-  editfield.showx<<-1
-  editfield.cursory<<-1
-  editfield.cursorx<<-2
 }
 
 load.editfield <- function(tiedosto) {
@@ -89,7 +85,7 @@ load.editfield <- function(tiedosto) {
     if (!identical(tt2[1],"S   ")) {
       rivi<-as.numeric(tt2[1])
       pituus<-nchar(tt2[2])
-      tt3<-unlist(strsplit(tt2[2],'|'))
+      tt3<-unlist(strsplit(tt2[2],NULL))
       for (i in 1:pituus) editfield[rivi,i]<<-tt3[i]
     }
     pos<-seek(filecon, rw="r")
@@ -97,8 +93,55 @@ load.editfield <- function(tiedosto) {
 #    tt <- iconv(tt, "CP850","ISO8859-1")
   }
   close.connection(filecon)
-  print.editfield(1,1)
-  setCursor(c(2,8))
+  cursor<-getCursor()
+  print.editfield(editfield.showx,editfield.showy)
+  setCursor(c(cursor[1],cursor[2]))
+}
+
+save.editfield <- function(tiedosto) {
+  filecon<-file(tiedosto, open="w", encoding="CP850")
+  otsikko <- paste("SURVO 98 edit field:",editfield.width,editfield.rows,editfield.shadowrows,"(32 bit version)")
+#  writeLines(otsikko,filecon,sep="\r\n")
+  writeLines(otsikko,filecon,sep="\n")
+  for (i in 1:editfield.rows) {
+    editline<-paste(editfield[i,][1:(editfield.width)],sep="",collapse="")
+    editline<-sub(' +$','', editline)
+    if (nchar(editline)>1) {
+      editline<-paste(i,"|",editline,sep="")
+#      writeLines(editline,filecon,sep="\r\n")
+      writeLines(editline,filecon,sep="\n")
+    }
+  }
+  close.connection(filecon)
+}
+
+load.dump <- function(tiedosto) {
+  filecon<-file(tiedosto, open="r", encoding="CP850")
+  muste.dump<<-readLines(filecon)
+  close.connection(filecon)
+}
+
+save.dump <- function(tiedosto) {
+  filecon<-file(tiedosto, open="w", encoding="CP850")
+  muste.dump[3]<<-as.character(editfield.cursory)
+  muste.dump[8]<<-as.character(editfield.cursorx-1)
+#  writeLines(muste.dump,filecon,sep="\r\n")
+  writeLines(muste.dump,filecon,sep="\n")
+
+
+#  alkurivi<-as.character(c(0,0,editfield.cursory,1,0,0,0,(editfield.cursorx-1)))
+#  writeLines(alkurivi,filecon,sep="\r\n")
+#  tokarivi<-as.character(c(0,0,0,"C:\\SURVO\\U\\D\\","C:","C:\\Survo\\U\\TMP\\RESULTS",0,"C:\\Survo\\S\\SURVO.TUT"))
+#  writeLines(tokarivi,filecon,sep="\r\n")
+#  for (k in 1:7) writeLines("0",filecon,sep="\r\n")
+#  writeLines("",filecon,sep="\r\n")
+#  kolmasrivi<-as.character(c(0,0,0,2,7,70))
+#  writeLines(kolmasrivi,filecon,sep="\r\n")
+#  for (k in 1:277) writeLines("0",filecon,sep="\r\n")
+#  writeLines(c("C:\\muste\\survo\\ASURVOMM.EDT","C:\\Survo\\U\\"),filecon,sep="\r\n")
+#  for (k in 1:14) writeLines("0",filecon,sep="\r\n")
+#  writeLines(c("","0","",""),filecon,sep="\r\n")
+  close.connection(filecon)
 }
 
 aktivointi <- function() {
@@ -106,32 +149,79 @@ aktivointi <- function() {
   rivi <- cursor[1] # strsplit(as.character(tkindex(txt,"insert")),"\\.")[[1]][1]
   rivi.alku <- paste(rivi,".8",sep='')
   rivi.loppu <- paste(rivi,".end",sep='')
-  input <- gsub('\n','',gsub('  +*',' ',tclvalue(tkget(txt,rivi.alku,rivi.loppu))))
+  input <- toupper(gsub('\n','',gsub('  +*',' ',tclvalue(tkget(txt,rivi.alku,rivi.loppu)))))
   komentosanat <- unlist(strsplit(input,' ')) #  cat("Komentosanat",komentosanat[1],komentosanat[2],sep='\n')
 
   if (identical(substr(input,1,2),'R>')) {                        # R-komento
     code=substr(input,3,nchar(input))
     execute(code)
   } else
-  if (identical(komentosanat[1],'MAT')) {                    # Matriisikomennot
+  if (identical(komentosanat[1],'MAT')) {                         # Matriisikomennot
+    if (identical(komentosanat[2],'LOAD')) {
+       matcode<-paste("apumat<-mat",komentosanat[3],sep="")
+       apumat<-eval(parse(text=matcode),envir=muste)
+       matnimi<-paste("MATRIX",komentosanat[3],"///")
+       matheader<-unlist(strsplit(matnimi,NULL))
+       editfield[(editfield.cursory+1),2:(nchar(matnimi)+1)]<<-matheader
+       for (i in 1:nrow(apumat)) {
+         rivi<-paste(apumat[i,],sep="",collapse=" ")
+         eririvi<-unlist(strsplit(rivi,NULL))
+         editfield[(editfield.cursory+1+i),2:(nchar(rivi)+1)]<<-eririvi
+       }
+       getCursor()
+       print.editfield(editfield.showx,editfield.showy)
+       setCursor(c(cursor[1],cursor[2]))
+    }
     komento <- unlist(strsplit(komentosanat[2],'='))
     if (!is.na(komento[2])) {
       dest <- komento[1]
       oikea <- unlist(strsplit(komento[2],'\\('))
       if (!is.na(oikea[2])) {
         param <- unlist(strsplit(gsub('\\)','',oikea[2]),','))
-        if (identical(oikea[1],'CON')) {
+        if (identical(oikea[1],'CON')) {                          # MAT A=CON(5,5,0.3)
           if(is.na(param[3])) { param[3] <- 1 }
           code <- paste('mat',dest,'<-matrix(',param[3],',',param[1],',',param[2],')',sep='')
           execute(code)
         }
       }
+      oikea <- unlist(strsplit(komento[2],'\\*'))                # MAT B=A*A
+      if (!is.na(oikea[2])) {
+        code <- paste('mat',dest,'<-mat',oikea[1],'%*%mat',oikea[2],sep='')
+        execute(code)
+      }
     }
   } else
   if (identical(komentosanat[1],'LOAD')) {                    # LOAD
     tiedosto<-paste(komentosanat[2],".EDT",sep="")
+    setCursor(c(2,8))
+    editfield.showx<<-1
+    editfield.showy<<-1
+    editfield.cursorx<<-2
+    editfield.cursory<<-1
     load.editfield(tiedosto)
+  } else
+  if (identical(komentosanat[1],'SAVE')) {                    # SAVE
+    tiedosto<-paste(komentosanat[2],".EDT",sep="")
+    save.editfield(tiedosto)
+  } else {
+#  if (identical(komentosanat[1],'NTERM')) {                    # NTERM
+    tiedosto<-"ASURVOMM.EDT"
+    save.editfield(tiedosto)
+    dump<-"ASURVOMM.DMP"
+    save.dump(dump)
+
+#    moduli<-paste("wine _",komentosanat[1],".exe C:/muste/survo/A A",sep="")  # Linux
+    moduli<-paste("_",komentosanat[1],".exe D:/survo/muste/A A",sep="")   # Windows
+
+    komento<-paste("system('",moduli,"', wait=FALSE)",sep="")
+    eval(parse(text=komento),envir=muste)
+#    system("wine _nterm.exe C:/muste/survo/A A", wait=FALSE)
+    Sys.sleep(1)
+#    load.dump("ASURVOMM.DMP")
+    load.editfield("ASURVOMM.EDT")
   }
+
+
 }
 
 checkeditboundaries <- function() {
@@ -143,7 +233,8 @@ checkeditboundaries <- function() {
   if(editfield.showx>(editfield.width-editarea.width)) editfield.showx<<-(editfield.width-editarea.width)
   if(editfield.showy>(editfield.rows-editarea.height+1)) editfield.showy<<-(editfield.rows-editarea.height+1)
   if(editfield.showx<1) editfield.showx<<-1
-  if(editfield.showy<1) editfield.showy<<-1 }
+  if(editfield.showy<1) editfield.showy<<-1
+}
 
 OnKey <- function(A,K) {
   cursor <- getCursor()
@@ -156,7 +247,10 @@ OnKey <- function(A,K) {
   if (identical(A,"")) return()
 
   merkki <- iconv(A, "UTF-8","")
-  if (is.na(merkki)) merkki="€" #return()
+  if (is.na(merkki)) {
+    koodi<-"merkki<-'\u20ac'" #return()
+    eval(parse(text=koodi))
+  }
 
   editfield[editfield.cursory,editfield.cursorx]<<-substr(merkki,1,1)
   print.editline(editfield.showx,editfield.showy)
@@ -296,7 +390,7 @@ OnHome <- function() {
       editfield.cursorx<<-2
       checkeditboundaries()
       print.editfield(editfield.showx,editfield.showy)
-      setCursor(c(cursor[1],(cursor[2])))
+      setCursor(c(cursor[1],cursor[2]))
       return()
     }
     setCursor(c(cursor[1],8))
@@ -309,8 +403,43 @@ OnHome <- function() {
   checkeditboundaries()
 }
 
+OnEnd <- function() {
+  cursor<-getCursor()
+  alkupaikka<-editfield.showx
+  editline<-paste(editfield[editfield.cursory,][1:(editfield.width)],sep="",collapse="")
+  editline<-sub(' +$','', editline)
+  pituus<-nchar(editline, type="chars")
+  sivu=floor(pituus/editarea.width)
+  editfield.cursorx<<-(pituus+1)
+  paikka<-(sivu*editarea.width)+1
+  if (pituus>=editfield.width) paikka<-paikka-1
+  editfield.showx<<-paikka
+  checkeditboundaries()
+  if (sivu>0 | alkupaikka>1) print.editfield(editfield.showx,editfield.showy)
+  kursorinpaikka<-(pituus-editfield.showx+8)
+  if (pituus>=editfield.width) kursorinpaikka<-(kursorinpaikka-1)
+  setCursor(c(cursor[1],kursorinpaikka))
+}
+
+OnControlEnd <- function() {
+  cursor<-getCursor()
+  pituus<-(editfield.width-editfield.cursorx)
+  for (i in 1:pituus) editfield[editfield.cursory,(editfield.cursorx-1+i)]<<-" "
+  print.editline(editfield.showx,editfield.showy)
+  setCursor(c(cursor[1],cursor[2]))
+}
+
+
+
+
 OnBackSpace <- function() {
   if (editfield.cursorx>2) {
+    if (editfield.cursorx>=editfield.width) {
+      if (!identical(editfield[editfield.cursory,(editfield.cursorx)]," ")) {
+        OnKey(" "," ")
+        return()
+      }
+    }
     OnLeft()
     OnKey(" "," ")
     OnLeft()
@@ -319,13 +448,13 @@ OnBackSpace <- function() {
 
 OnDel <- function() {
  cursor<-getCursor()
- if(identical(editfield.cursorx,editfield.width)) editfield[editfield.cursory,editfield.cursorx]<<-" "
+ if(editfield.cursorx>=editfield.width) return() # editfield[editfield.cursory,editfield.cursorx]<<-" "
  else {
    editline<-paste(editfield[editfield.cursory,][(editfield.cursorx+1):(editfield.width)],sep="",collapse="")
    editline<-paste(editline," ",sep="")
 # print(editline)
-   pituus<-nchar(editline)
-   chars<-unlist(strsplit(editline,'|')) 
+   pituus<-nchar(editline, type="chars")
+   chars<-unlist(strsplit(editline,NULL)) 
    for (i in 1:pituus) editfield[editfield.cursory,(editfield.cursorx-1+i)]<<-chars[i]
  }
  print.editline(editfield.showx,editfield.showy)
@@ -342,21 +471,39 @@ OnPehmoEnter <- function()
 OnPehmoLeave <- function()
 {
         tkdelete(txt,"27.0","27.end")
-        tkinsert(txt,"27.0","Kokeilua TCL/TK:lla","note") }
+        tkinsert(txt,"27.0","Kokeilua TCL/TK:lla","note")
+}
 
+RightClick <- function(x,y) # x and y are the mouse coordinates
+{ 
+  cat("Right click:",as.integer(x),as.integer(y))
+  rootx <- as.integer(tkwinfo("rootx",txt))
+  rooty <- as.integer(tkwinfo("rooty",txt))
+  xTxt <- as.integer(x)+rootx
+  yTxt <- as.integer(y)+rooty 
+} 
 
+muste <<- environment()
 ikkuna <<- tktoplevel()
 tkwm.title(ikkuna, "MUSTE")
-txt <<- tktext(ikkuna,width=80,height=27,foreground="#000000",background="#FEFEFE",wrap="none",font="courier")
+fixedfont <- tkfont.create(family="Courier",size=9)
+txt <<- tktext(ikkuna,width=80,height=27,foreground="#000000",background="#FEFEFE",wrap="none",font=fixedfont)
 tkgrid(txt)
 editarea.height<<-23
 editarea.width<<-72
+
+#tkfont.configure(fixedfont,family="Courier",size=9)
 
 # Poistetaan text-widgetin perussidokset käytöstä
 sidokset <- gsub("Text ","",tclvalue(tkbindtags(txt)))
 tkbindtags(txt,sidokset)
 
+load.dump("MUSTE.DMP")
 create.editfield(101,300,300)
+editfield.showy<<-1
+editfield.showx<<-1
+editfield.cursory<<-1
+editfield.cursorx<<-2
 print.editfield(1,1)
 setCursor(c(2,8))
 
@@ -365,6 +512,8 @@ tkbind(txt,"<Key>",OnKey)
 tkbind(txt,"<Next>",OnPageDown)
 tkbind(txt,"<Prior>",OnPageUp)
 tkbind(txt,"<Home>",OnHome)
+tkbind(txt,"<End>",OnEnd)
+tkbind(txt,"<Control-End>",OnControlEnd)
 tkbind(txt,"<Down>",OnDown)
 tkbind(txt,"<Up>",OnUp)
 tkbind(txt,"<Left>",OnLeft)
@@ -375,15 +524,6 @@ tkbind(txt,"<BackSpace>",OnBackSpace)
 tkbind(txt,"<Delete>",OnDel)
 tktag.bind(txt,"pehmo","<Enter>",OnPehmoEnter)
 tktag.bind(txt,"pehmo","<Leave>",OnPehmoLeave)
-
-RightClick <- function(x,y) # x and y are the mouse coordinates
-{ 
-  cat("Right click:",as.integer(x),as.integer(y))
-  rootx <- as.integer(tkwinfo("rootx",txt))
-  rooty <- as.integer(tkwinfo("rooty",txt))
-  xTxt <- as.integer(x)+rootx
-  yTxt <- as.integer(y)+rooty 
-} 
 tkbind(txt, "<Button-3>",RightClick)
 
 
