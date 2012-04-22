f7c80dcb4cdd98775c91685fe589074b1ee9a37a
sund <sund@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-04-22 04:41:38 +0000

    rftest2 update

---
+++ pkg/rftest2/R/rftest.R
@@ -0,0 +1,15 @@
+rftest <- function() 
+{
+#requireNamespace("tcltk",quietly=TRUE)
+#attachNamespace("tcltk")
+
+.rftest$ikkuna <- tktoplevel()
+tkwm.title(.rftest$ikkuna, "Rftest")
+.rftest$font <- tkfont.create(family="Courier",size=12) }
+.rftest$txt <- tktext(.rftest$ikkuna,width=80,height=25,foreground="#000000",background="snow",wrap="none",font=.rftest$font,undo=FALSE)
+ tkgrid(.rftest$txt)  
+
+Sys.sleep(10);
+tkdestroy(.rftest$txt)
+tkdestroy(.rftest$ikkuna)
+}
