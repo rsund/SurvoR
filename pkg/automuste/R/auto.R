12c9a6d1f9641c6f8acdcec612a40421452a9033
Reijo Sund <reijo.sund@helsinki.fi>
2019-06-09 13:41:38 +0300

    Automuste 0.1.0
    Autostart and autoupdate for Survo R

---
+++ pkg/automuste/R/auto.R
@@ -0,0 +1,27 @@
+.onLoad <- function(libname,pkgname) { automuste() }
+
+automuste <- function(kysy=FALSE,curl="http://www.survo.fi/muste")
+    {
+    workdir <- getwd()
+    newpkgs <- new.packages(contriburl=curl,ask=kysy)
+    if (length(newpkgs>0))
+        try(install.packages(newpkgs,contriburl=curl))
+    updpkgs <- as.vector(available.packages(contriburl=curl)[,"Package"])
+    intpkgs <- intersect(.packages(),updpkgs)
+    if (length(intpkgs)>0)
+        warning(paste("Package(s)",intpkgs,"loaded - skipping update."))
+    updpkgs <- setdiff(updpkgs,.packages())
+    if (length(updpkgs>0))
+        try(update.packages(contriburl=curl,oldPkgs=updpkgs,ask=kysy))
+    if(length(intersect(ls(all.names=TRUE),".First"))==0)
+        {
+        tmp <- tempfile()
+		cat(".First <- function() require(automuste)",file=tmp,sep="\n",append=TRUE)
+        if (!file.rename(tmp,paste(workdir,"/.Rprofile",sep="")))
+            {
+            file.remove(tmp)
+            warning("Could not create .Rprofile for autostart!")
+            }
+        }
+    if (require("muste")) if (interactive()) muste()
+    }
